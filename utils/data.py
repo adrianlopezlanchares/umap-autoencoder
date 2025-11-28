c9344703bad22a6443b6ffc709318db637c04405
adrianlopezlanchares <202107370@alu.comillas.edu>
2025-11-28 10:35:39 +0100

    get image np arrays function

--- utils/data.py
+++ utils/data.py
@@ -3,6 +3,7 @@ import zipfile
 from pathlib import Path
 
 import numpy as np
+from PIL import Image
 import torch
 from torch.utils.data import Dataset
 
@@ -41,6 +42,39 @@ def download_data() -> None:
     output_file.unlink()
 
     print("Done")
+
+
+def get_image_np_arrays(process_size: float = 0.01) -> np.ndarray:
+    """
+    Load images from the CelebA dataset and return them as a numpy array.
+    The images are resized to 64x64 pixels.
+
+    Args:
+        process_size: Percentage of files to process. Between 0 and 1. Defaults to 0.01.
+
+    Returns:
+        np.ndarray: Array of images with shape (N, 64, 64, 3)
+    """
+    project_root = Path(__file__).resolve().parents[1]
+    data_dir = project_root / "data"
+
+    imgs_dir = data_dir / "img_align_celeba" / "img_align_celeba"
+    paths = sorted([p for p in imgs_dir.iterdir() if p.suffix.lower() == ".jpg"])
+    n = int(len(paths) * process_size)
+    paths = paths[:n]
+
+    print("Loading images...")
+    imgs = []
+    i = 1
+    for p in paths:
+        print(f"Image {i}/{len(paths)}            ", end="\r")
+        i += 1
+        img = Image.open(p).convert("RGB").resize((64, 64))
+        imgs.append(np.array(img, dtype=np.float32) / 255.0)
+    imgs = np.stack(imgs, axis=0)  # (N, H, W, 3)
+    print(f"{len(imgs)} images loaded")
+
+    return imgs
 
 
 class UMAPImageDataset(Dataset):
