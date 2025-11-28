da46d7c86334a39695a2d43e5ad1ffc794c2edbf
adrianlopezlanchares <202107370@alu.comillas.edu>
2025-11-28 15:12:47 +0100

    generalize numpy array obtention function

--- utils/data.py
+++ utils/data.py
@@ -44,12 +44,49 @@ def download_celeba_data() -> None:
     print("Done")
 
 
-def get_celeba_image_np_arrays(process_size: float = 0.01) -> np.ndarray:
+def download_mnist_data() -> None:
+    """
+    Download the MNIST dataset from Kaggle to the project root, under /data.
+    """
+    project_root = Path(__file__).resolve().parents[1]
+    data_dir = project_root / "data"
+    data_dir.mkdir(exist_ok=True)
+
+    img_folder = data_dir / "mnist"
+    output_file = data_dir / "mnist-dataset.zip"
+
+    if img_folder.exists():
+        print("Data is already downloaded. Skipping download.")
+        return
+
+    subprocess.run(
+        [
+            "curl",
+            "-L",
+            "-o",
+            str(output_file),
+            "https://www.kaggle.com/api/v1/datasets/download/hojjatk/mnist-dataset",
+        ],
+        check=True,
+    )
+
+    print("Unzipping dataset...")
+    with zipfile.ZipFile(output_file, "r") as z:
+        z.extractall(data_dir)
+
+    print("Deleting zip...")
+    output_file.unlink()
+
+    print("Done")
+
+
+def get_image_np_arrays(dataset: str, process_size: float = 0.01) -> np.ndarray:
     """
     Load images from the CelebA dataset and return them as a numpy array.
     The images are resized to 64x64 pixels.
 
     Args:
+        dataset: Name of the dataset to use. Supported: "celeba", "mnist".
         process_size: Percentage of files to process. Between 0 and 1. Defaults to 0.01.
 
     Returns:
@@ -69,8 +106,13 @@ def get_celeba_image_np_arrays(process_size: float = 0.01) -> np.ndarray:
     for p in paths:
         print(f"Image {i}/{len(paths)}            ", end="\r")
         i += 1
-        img = Image.open(p).convert("RGB").resize((64, 64))
+
+        img = Image.open(p).convert("RGB")
+        if dataset == "celeba" or dataset == "img_align_celeba":
+            img = img.resize((64, 64))
+
         imgs.append(np.array(img, dtype=np.float32) / 255.0)
+
     imgs = np.stack(imgs, axis=0)  # (N, H, W, 3)
     print(f"{len(imgs)} images loaded")
 
