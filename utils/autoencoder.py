6681d4685c8d667b6b873142d7edb6737cacee24
adrianlopezlanchares <202107370@alu.comillas.edu>
2025-11-28 11:54:41 +0100

    initial commit

---
+++ utils/autoencoder.py
@@ -0,0 +1,26 @@
+import torch
+
+
+class Autoencoder(torch.nn.Module):
+    def __init__(self, input_dim: int, embedding_dim: int):
+        super(Autoencoder, self).__init__()
+        self.encoder = torch.nn.Sequential(
+            torch.nn.Linear(input_dim, 512),
+            torch.nn.ReLU(),
+            torch.nn.Linear(512, 256),
+            torch.nn.ReLU(),
+            torch.nn.Linear(256, embedding_dim),
+        )
+        self.decoder = torch.nn.Sequential(
+            torch.nn.Linear(embedding_dim, 256),
+            torch.nn.ReLU(),
+            torch.nn.Linear(256, 512),
+            torch.nn.ReLU(),
+            torch.nn.Linear(512, input_dim),
+            torch.nn.Sigmoid(),  # Assuming input images are normalized between 0 and 1
+        )
+
+    def forward(self, x: torch.Tensor) -> torch.Tensor:
+        encoded = self.encoder(x)
+        decoded = self.decoder(encoded)
+        return decoded
